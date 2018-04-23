69e8674a398b744ad13ee47256e372bdc27d368d
jimmyrleung <jimmy.rl19@hotmail.com>
2018-04-23 17:53:38 -0300

    Aula 1 e 2

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,46 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, LoadingController, AlertController } from 'ionic-angular';
+import { Carro } from '../../models/carro';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http'
+import { CarrosServiceProvider } from '../../providers/carros-service/carros-service';
+import { NavLifecycles } from '../../utils/ionic/nav/nav-lifecycle';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
+export class HomePage implements NavLifecycles {
 
-  constructor(public navCtrl: NavController) {
+  public carros: Array<Carro> = [];
 
+  constructor(public navCtrl: NavController,
+    private _carrosService: CarrosServiceProvider,
+    private _loadingCtrl: LoadingController,
+    private _alertCtrl: AlertController) {
+  }
+
+  ionViewDidLoad() {
+    let loading = this._loadingCtrl.create({
+      content: "Carregando lista de carros..."
+    });
+
+    loading.present();
+
+
+    this._carrosService.lista()
+      .subscribe((carros) => {
+        this.carros = carros;
+      }, (err: HttpErrorResponse) => {
+        console.log(err);
+
+        this._alertCtrl.create({
+          title: "Falha na conexão",
+          subTitle: "Não foi possível carregar a lista de carros. Tente novamente mais tarde.",
+          buttons: [{ text: "Ok" }]
+        }).present();
+
+        loading.dismiss()
+      }, () => loading.dismiss());
   }
 
 }
