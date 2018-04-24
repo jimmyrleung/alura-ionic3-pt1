ade876bbfe062bed1e9df06ac5c03f81a995c136
jimmyrleung <jimmy.rl19@hotmail.com>
2018-04-23 22:09:07 -0300

    acessórios em lista fixa com toggle

--- src/pages/escolha/escolha.ts
+++ src/pages/escolha/escolha.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { Carro } from '../../models/carro';
+import { Acessorio } from '../../models/acessorio';
 
 @IonicPage()
 @Component({
@@ -10,8 +11,24 @@ import { Carro } from '../../models/carro';
 export class EscolhaPage {
 
   public carro: Carro;
+  public acessorios = [];
+  public _precoTotal: number;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     this.carro = this.navParams.get('carroSelecionado');
+    this._precoTotal = this.carro.preco;
+    this.acessorios = [
+      { nome: "Freio ABS", preco: 800 },
+      { nome: "Ar-condicionado", preco: 1000 },
+      { nome: "MP3 Player", preco: 500 }
+    ]
+  }
+
+  atualizaTotal(ativado: boolean, acessorio: Acessorio) {
+    this._precoTotal = ativado ? this._precoTotal + acessorio.preco : this._precoTotal - acessorio.preco;
+  }
+
+  get precoTotal() {
+    return this._precoTotal;
   }
 }
